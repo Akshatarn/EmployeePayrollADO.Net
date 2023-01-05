76c796a5190f6a702cf0821b5b872599766b8383
akshata_rn <akshatarn123@gamail.com>
2023-01-05 20:33:51 +0530

    Adding New employee into table

--- EmployeePayrollADO_Net/Program.cs
+++ EmployeePayrollADO_Net/Program.cs
@@ -12,7 +12,20 @@ namespace EmployeePayrollADO_Net
         {
             EmployeeRepo employeeRepository = new EmployeeRepo();
             employeeRepository.CheckConnection();   //UC1 Checking Connectivity Status with the DataBase.
-            employeeRepository.GetAllEmployee();
+            employeeRepository.GetAllEmployee();    //UC2 Retrieve all data from Table.
+            EmpModel employeeModel = new EmpModel();
+            employeeModel.Name = "Sparsha";
+            employeeModel.Salary = 40000;
+            employeeModel.StartDate = DateTime.Now;
+            employeeModel.Gender = "M";
+            employeeModel.empPhone = 86682721;
+            employeeModel.Address = "karnataka";
+            employeeModel.Department = "HR";
+            employeeModel.Deductions = 564.56;
+            employeeModel.Taxable_Pay = 1300;
+            employeeModel.Income_Tax = 13400;
+            employeeModel.Net_Pay = 33333.34;
+            employeeRepository.AddEmployee(employeeModel);
         }
     }
 }
